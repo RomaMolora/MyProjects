6069d7d0544fc8043d2edde2479d499f1a1ef14d
Roma <kgspsoad@gmail.com>
2021-03-20 11:28:59 +0300

    add commets

--- stopwatch.py
+++ stopwatch.py
@@ -1,96 +1,107 @@
 ﻿from tkinter import *
 import datetime
 import csv
+import time
 
 
 class Window:
+    """Create window"""
+
     def __init__(self):
         self.root = Tk()
-        self.__temp = 0
-        self.__id = ''
-        self.save = []
+        self.__temp = 0  # time
+        self.__id = ''  # I have no idea, but without it it doesn't work
 
-        self.l = Label(self.root,
-                       font=('Ubuntu', 100),
-                       text='00:00:00'
-                       )
+        self.time = Label(self.root,
+                          font=('Ubuntu', 100),
+                          text='00:00:00'
+                          )
 
-        self.b = Button(self.root,
-                        command=self.start,
-                        text='start'
-                        )
+        self.start = Button(self.root,
+                            command=self.start,
+                            text='start'
+                            )
 
-        self.b_stop = Button(self.root,
-                             command=lambda: self.stop(),
-                             text='stop'
-                             )
+        self.stop_ = Button(self.root,
+                            command=lambda: self.stop(),
+                            text='stop'
+                            )
 
-        self.b_cont = Button(self.root,
-                             command=lambda: self.cont(),
-                             text='continue'
-                             )
+        self.continue_ = Button(self.root,
+                                command=lambda: self.cont(),
+                                text='continue'
+                                )
 
-        self.b_save = Button(self.root,
-                             text='save',
-                             command=lambda: self.save_()
-                             )
+        self.save = Button(self.root,
+                           text='save',
+                           command=lambda: self.save_()
+                           )
 
-        self.packs()
+        self.packs()  # accommodation packs this method
 
     def packs(self):
-        self.l.pack()
-        self.b.pack()
+        """packs one label(time) and 4 buttons"""
+        self.time.pack()
+        self.start.pack()
 
     def start(self):
-        self.b.pack_forget()
-        self.b_stop.pack()
-        self.tick()
+        """start timer"""
+        self.start.pack_forget()  # hide button start
+        self.stop_.pack()  # show button stop
+        self.tick()  # recursion method tick
 
     def stop(self):
-        self.root.after_cancel(self.__id)
-        self.b_stop.pack_forget()
-        self.b_cont.pack()
-        self.b_save.pack()
+        """stop timer"""
+        self.root.after_cancel(self.__id)  # something like a while loop(stops him)
+        self.stop_.pack_forget()  # hide button stop
+        self.continue_.pack()  # show button continue
+        self.save.pack()  # show button save
 
     def cont(self):
-        self.tick()
-        self.b_cont.pack_forget()
-        self.b_stop.pack()
-        self.b_save.pack_forget()
+        """continue work this program"""
+        self.tick()  # recursion method tick
+        self.continue_.pack_forget()  # hide button continue
+        self.stop_.pack()  # show button stop
+        self.save.pack_forget()  # hide button save
 
     def save_(self):
-        today = datetime.date.today()
-        today = str(today)
-        list_today = [today]
-        self.temp_replace = str(int(self.__temp / 60 / 60))
+        """saves time to file with csv extension"""
+        today = datetime.date.today()  # today date
+        today = str(today)  # transform in type str
+        list_today = [today]  # append
+        self.temp_replace = str(int(self.__temp / 60 / 60))  # int hours
 
+        # add a normal view of the result to the file
         self.f_temp = str(self.f_temp)
         self.f_temp = list(self.f_temp)
         self.f_temp.pop(1)
         self.f_temp.insert(1, self.temp_replace)
         self.f_temp = ''.join(self.f_temp)
 
+        # this to add to the csv file
         list_time = [self.f_temp]
         list_everything = []
         list_everything.extend(list_today)
         list_everything.extend(list_time)
 
+        # open csv file
         with open('Time work.csv', 'a') as f:
             w = csv.writer(f, delimiter=',')
             w.writerow(list_everything)
 
     def run(self):
+        """run program"""
         self.root.mainloop()
 
     def tick(self):
-        import time
+        """something like a while loop"""
         time.sleep(1)
         self.__id = self.root.after(1, self.tick)
-        self.f_temp = datetime.datetime.fromtimestamp(self.__temp).strftime('%H:%M:%S')
-        self.l.configure(text=str(self.f_temp))
+        self.f_temp = datetime.datetime.fromtimestamp(self.__temp).strftime('%H:%M:%S')  #
+        self.time.configure(text=str(self.f_temp))  # replace text label time
         self.__temp += 1
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # main
     w = Window()
-    w.run()
+    w.run()  # run
