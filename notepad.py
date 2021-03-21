103f4d74e90ae62c5165ad24ad5ae182721c8f22
Roma <kgspsoad@gmail.com>
2021-03-21 15:25:21 +0300

    ~

---
+++ notepad.py
@@ -0,0 +1,153 @@
+# from tkinter import *
+# from tkinter import scrolledtext
+# from tkinter import Menu
+#
+# w = Tk()
+# txt = scrolledtext.ScrolledText(w, width=40, height=10)
+# txt.insert(INSERT, 'Entry')
+# # txt.pack()
+# b = Button(w, command=lambda: clear())
+# # b.pack()
+#
+# def clear():
+#     txt.delete(1.0, END)
+#
+#
+# menu = Menu(w)
+# new_item = Menu(menu)
+# new_item2 = Menu(menu)
+# new_item.add_command(label='view file', command=lambda: [txt.pack(), b.pack()])
+# new_item.add_command(label='remove', command=lambda: [txt.pack_forget(),
+#                                                       b.pack_forget()])
+# new_item.add_command(label='clear', command=lambda: clear())
+# new_item.add_command(label='new file', command=lambda: [clear(), txt.insert(INSERT, 'Entry')])
+# menu.add_cascade(label='New File', menu=new_item)
+#
+# new_item2.add_command(label='edit', command=lambda: [clear(), txt.insert(INSERT, 'New File')])
+# menu.add_cascade(label='Edit', menu=new_item2)
+#
+# # menu.add_command(label='edit', command=lambda: [txt.pack_forget(), b.pack_forget()])
+#
+#
+#
+# # new_item.add_command(label='new file', command=lambda: [txt.pack(), b.pack()])
+# # menu.add_cascade(label='new file')
+#
+# w.config(menu=menu)
+#
+#
+#
+#
+# w.mainloop()
+
+
+# from tkinter import *
+#
+# root = Tk()
+# root.wm_attributes("-alpha", 0.8)
+# # root.overrideredirect(True)  # делает окно неподвижным
+# # root.wm_attributes("-topmost", 0.8)
+#
+# # root.config(bg='systemTransparent')
+#
+# # root.geometry("+250+250")
+# # button = Button(root, text='example')
+# # button.pack()
+#
+# root.mainloop()
+
+
+from tkinter import *
+#
+# TF = True
+#
+# def about():
+#     a = Toplevel()
+#     a.geometry('200x150')
+#     a['bg'] = 'grey'
+#     a.overrideredirect(TF)
+#     Label(a, text="About this") \
+#         .pack(expand=1)
+#     a.after(5000, lambda: a.destroy())
+#
+#
+# root = Tk()
+# root.title("Главное окно")
+# # TF = False
+# Button(text="Button", width=20).pack()
+# Label(text="Label", width=20, height=3) \
+#     .pack()
+# Button(text="About", width=20, command=about) \
+#     .pack()
+#
+# root.mainloop()
+
+
+#custom title bar for tkinter
+
+# from tkinter import *
+#
+# root=Tk()
+# root.overrideredirect(True) # turns off title bar, geometry
+# root.geometry('400x100+200+200') # set new geometry
+#
+# # make a frame for the title bar
+# title_bar = Frame(root, bg='#2e2e2e', relief='raised', bd=2,highlightthickness=0)
+#
+# # put a close button on the title bar
+# close_button = Button(title_bar, text='X', command=root.destroy,bg="#2e2e2e",padx=2,pady=2,activebackground='red',bd=0,font="bold",fg='white',highlightthickness=0)
+#
+# # a canvas for the main area of the window
+# window = Canvas(root, bg='#2e2e2e',highlightthickness=0)
+#
+# # pack the widgets
+# title_bar.pack(expand=1, fill=X)
+# close_button.pack(side=RIGHT)
+# window.pack(expand=1, fill=BOTH)
+# xwin=None
+# ywin=None
+# # bind title bar motion to the move window function
+#
+# def move_window(event):
+#     root.geometry('+{0}+{1}'.format(event.x_root, event.y_root))
+# def change_on_hovering(event):
+#     global close_button
+#     close_button['bg']='red'
+# def return_to_normalstate(event):
+#     global close_button
+#     close_button['bg']='#2e2e2e'
+#
+#
+# title_bar.bind('<B1-Motion>', move_window)
+# close_button.bind('<Enter>',change_on_hovering)
+# close_button.bind('<Leave>',return_to_normalstate)
+# root.mainloop()
+
+# from tkinter import *
+# # import tklMessageBox as messagebox
+# r = Tk()
+# a = 1
+# r.overrideredirect(0)
+# r.resizable(a, a)
+#
+# def press():
+#     r.resizable(0, 0)
+#     r.overrideredirect(1)
+#
+# b = Button(r, text='sdfsdfsdfsdf', command=press).pack()
+#
+# r.mainloop()
+
+
+# from tkinter import *
+# import time
+# root = Tk()
+# a = True
+# root.overrideredirect(a)
+# root.geometry('200x200+100+100')
+# root.resizable(False, False)
+# root.update_idletasks()
+# time.sleep(1)
+# print(1)
+# a = False
+# root.mainloop()
--- stopwatch.py
+++ stopwatch.py
@@ -6,14 +6,25 @@ import time
 
 class Window:
     """Create window"""
-
     def __init__(self):
+        # self.move_window = True
         self.root = Tk()
+
+        self._saved = Label(self.root, text='saved')
+
+        self.root.resizable(0, 0)
+        self.root.title('tick tack')
+
+        # self.root.overrideredirect(self.move_window)
+
+        self.root.wait_visibility(self.root)
+        self.root.attributes("-alpha", 0.9)
+
         self.__temp = 0  # time
         self.__id = ''  # I have no idea, but without it it doesn't work
 
         self.time = Label(self.root,
-                          font=('Ubuntu', 100),
+                          font=('Ubuntu', 50),
                           text='00:00:00'
                           )
 
@@ -36,6 +47,11 @@ class Window:
                            text='save',
                            command=lambda: self.save_()
                            )
+        #
+        # self.move_ = Button(self.root,
+        #                     text='move',
+        #                     command=lambda: self.move()
+        #                     ).pack()
 
         self.packs()  # accommodation packs this method
 
@@ -43,6 +59,11 @@ class Window:
         """packs one label(time) and 4 buttons"""
         self.time.pack()
         self.start.pack()
+
+    # def move(self):
+    #     if str(self.move_) == '.!button5':
+    #         self.move_window = False
+    #         print(self.move_window)
 
     def start(self):
         """start timer"""
@@ -63,6 +84,7 @@ class Window:
         self.continue_.pack_forget()  # hide button continue
         self.stop_.pack()  # show button stop
         self.save.pack_forget()  # hide button save
+        self._saved.pack_forget()
 
     def save_(self):
         """saves time to file with csv extension"""
@@ -70,6 +92,7 @@ class Window:
         today = str(today)  # transform in type str
         list_today = [today]  # append
         self.temp_replace = str(int(self.__temp / 60 / 60))  # int hours
+        self._saved.pack()
 
         # add a normal view of the result to the file
         self.f_temp = str(self.f_temp)
@@ -92,6 +115,7 @@ class Window:
     def run(self):
         """run program"""
         self.root.mainloop()
+        self.root.wm_attributes("-alpha", 0.8)
 
     def tick(self):
         """something like a while loop"""
@@ -100,6 +124,11 @@ class Window:
         self.f_temp = datetime.datetime.fromtimestamp(self.__temp).strftime('%H:%M:%S')  #
         self.time.configure(text=str(self.f_temp))  # replace text label time
         self.__temp += 1
+
+
+# self.root.wait_visibility(self.root)
+# self.root.attributes("-alpha", 0.9)
+# root.geometry("+0+0")
 
 
 if __name__ == '__main__':  # main
