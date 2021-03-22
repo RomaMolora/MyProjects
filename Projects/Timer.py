6f8823b1d255f7a756031eb92f1a619d606f6240
Roma <kgspsoad@gmail.com>
2021-03-22 16:04:41 +0300

    add new functionals

---
+++ Projects/Timer.py
@@ -0,0 +1,134 @@
+from tkinter import *
+from tkinter import messagebox, ttk
+from tkinter.ttk import Progressbar
+import time, os, platform, threading
+
+root = Tk()
+m = messagebox.askyesnocancel('', '"yes(Да)" - english(Английский), "no(Нет)" - Русский(Russia)')
+
+if m == False:
+    a = 'Перезагрузите или выключите компьютер'
+    q = 'Если «Да» - выключить компьютер, если «Нет» - перезагрузить компьютер.'
+    c = 'Правила'
+    d = 'Если поле пустое - оставьте его пустым.'
+    f = 'Нажмите ОК и эта операция начнется'
+    g = 'Прогресс -'
+    h = 'Часы -'
+    m = 'Минуты -'
+    s = 'Секунды -'
+    p = 'Нажать Кнопку'
+
+elif m == True:
+    a = 'Restart or shutdown the computer '
+    q = 'If "yes" - shut down the computer, if "no" - restart the computer.'
+    c = 'Rules'
+    d = 'if the field is empty - leave the field blank.'
+    f = 'Enter \'ok\' and this operation will start'
+    g = 'Progress is - '
+    h = 'Hours - '
+    m = 'Minutes - '
+    s = 'Seconds - '
+    p = 'press button'
+
+else:
+    quit()
+
+messagebox.showinfo(c, d.title())
+
+root.wm_attributes('-alpha', 0.9)
+root.iconbitmap('')  # указать путь к иконке
+root.title(d.title())
+root.resizable(False, False)
+style = ttk.Style()
+style.theme_use('default')
+style.configure("black.Horizontal.TProgressbar", background='black')
+
+System = platform.system()
+
+
+def message():
+    return messagebox.askquestion('', f)
+
+
+def ent():
+    def down_progress(arg):
+        def progress():
+            n = 0
+            while True:
+                time.sleep(1)
+                bar['value'] += (200 / res) / 2
+                n += 1
+
+                if n == res:
+                    if System == 'Linux':
+                        if arg == 's':
+                            os.system('systemctl poweroff')
+                        elif arg == 'r':
+                            os.system('reboot')
+
+                    if System == 'Windows':
+                        if arg == 's':
+                            os.system('shutdown -s')
+                        elif arg == 'r':
+                            os.system('shutdown -r')
+
+        l_bar.grid(column=2, row=2)
+        bar.grid(column=3, row=2)
+        threading.Thread(target=progress).start()
+
+    if hour.get() == '':
+        h = 0
+    else:
+        h = int(hour.get())
+
+    if minutes.get() == '':
+        m = 0
+    else:
+        m = int(minutes.get())
+
+    if second.get() == '':
+        s = 1
+    else:
+        s = int(second.get())
+
+    if isinstance(h < 25, int) and h < 25 or h > 0:
+        if isinstance(m, int) and m < 61 or m > 0:
+            if isinstance(s, int) and s < 61 or s != 0 or s > 0:
+                if message() == 'yes':
+                    h *= 3600;
+                    m *= 60;
+                    s *= 1;
+                    res = h + m + s
+
+                    bar = Progressbar(root, length=200, style='green.Horizontal.TProgressbar')
+                    l_bar = Label(root, text=g)
+
+                    sms = messagebox.askyesnocancel(a, q)
+                    if sms == True: down_progress('s')
+                    if sms == False: down_progress('r')
+                    if sms == None: pass
+
+
+b = Button(root, text=p, command=ent)
+
+l = Label(root, text=' ')
+l_hour = Label(root, text=h)
+l_minutes = Label(root, text=m)
+l_second = Label(root, text=s)
+l_e = Label(root, text=' ')
+
+hour = Entry(root, width=2)
+minutes = Entry(root, width=2)
+second = Entry(root, width=2)
+
+l.grid(column=0, row=0)
+l_hour.grid(column=1, row=0)
+hour.grid(column=2, row=0)
+l_minutes.grid(column=3, row=0)
+minutes.grid(column=4, row=0)
+l_second.grid(column=6, row=0)
+second.grid(column=7, row=0)
+l_e.grid(column=8, row=0)
+b.grid(column=3, row=1)
+
+root.mainloop()
--- Projects/stopwatch.py
+++ Projects/stopwatch.py
@@ -1,4 +1,5 @@
-﻿from tkinter import *
+﻿import tkinter
+from tkinter import messagebox
 import datetime
 import csv
 import time
@@ -8,11 +9,19 @@ class Window:
     """Create window"""
 
     def __init__(self):
+        self.root = tkinter.Tk()
 
-        self.root = Tk()
-        # self.root.overrideredirect(True)
-        self.root.attributes("-topmost", True)
-        self._saved = Label(self.root, text='saved')
+        self.root.protocol("WM_DELETE_WINDOW", [
+            messagebox.showinfo('', 'Press "escape" to exit'),
+            self.root.iconify,
+        ])
+        self.root.bind('<Escape>', lambda a: self.root.destroy())
+
+        width = self.root.winfo_screenwidth()
+        height = self.root.winfo_screenheight()
+        self.root.geometry(f"+{width}+{height}")
+
+        self._saved = tkinter.Label(self.root, text='saved')
 
         self.root.resizable(0, 0)
         self.root.title('tick tack')
@@ -20,60 +29,59 @@ class Window:
         self.root.wait_visibility(self.root)
         self.root.attributes("-alpha", 0.9)
 
+        self.f_temp = ''
+
         self.__temp = 0  # time
         self.__id = ''  # I have no idea, but without it it doesn't work
+        self.time = tkinter.Label(self.root,
+                                  font=('Ubuntu', 30),
+                                  text='00:00:00'
+                                  )
 
-        self.time = Label(self.root,
-                          font=('Ubuntu', 30),
-                          text='00:00:00'
-                          )
+        self.start_button = tkinter.Button(self.root,
+                                           command=self.start_program,
+                                           text='start'
+                                           )
 
-        self.start = Button(self.root,
-                            command=self.start,
-                            text='start'
-                            )
+        self.stop_ = tkinter.Button(self.root,
+                                    command=self.stop,
+                                    text='stop'
+                                    )
 
-        self.stop_ = Button(self.root,
-                            command=lambda: self.stop(),
-                            text='stop'
-                            )
+        self.continue_ = tkinter.Button(self.root,
+                                        command=self.cont,
+                                        text='continue'
+                                        )
 
-        self.continue_ = Button(self.root,
-                                command=lambda: self.cont(),
-                                text='continue'
-                                )
+        self.save = tkinter.Button(self.root,
+                                   text='save',
+                                   command=self.save_
+                                   )
 
-        self.save = Button(self.root,
-                           text='save',
-                           command=lambda: self.save_()
-                           )
-
-        self.move_ = Button(self.root,
-                            text='move',
-                            command=lambda: self.move()
-                            )
+        self.move_ = tkinter.Button(self.root,
+                                    text='move',
+                                    command=self.move
+                                    )
 
         self.packs()  # accommodation packs this methods
 
     def packs(self):
         """packs one label(time) and 4 buttons"""
         self.time.pack()
-        self.start.pack()
+        self.start_button.pack()
         self.move_.pack()
 
     def move(self):
+        """move window"""
         if self.root.wm_overrideredirect():
             self.root.overrideredirect(False)
-            print('yes')
         else:
             self.root.overrideredirect(True)
-            print('no')
         self.root.update()
 
-
-    def start(self):
+    def start_program(self):
         """start timer"""
-        self.start.pack_forget()  # hide button start
+        self.start_button.pack_forget()  # hide button start
         self.stop_.pack()  # show button stop
         self.tick()  # recursion method tick
 
@@ -114,14 +122,12 @@ class Window:
         list_everything.extend(list_time)
 
         # open csv file
-        with open('Time work.csv', 'a') as f:
-            w = csv.writer(f, delimiter=',')
-            w.writerow(list_everything)
+        with open('Time work.csv', 'a') as time_work:
+            csv.writer(time_work, delimiter=',').writerow(list_everything)
 
     def run(self):
         """run program"""
         self.root.mainloop()
-        self.root.wm_attributes("-alpha", 0.8)
 
     def tick(self):
         """something like a while loop"""
@@ -138,5 +144,4 @@ class Window:
 
 
 if __name__ == '__main__':  # main
-    w = Window()
-    w.run()  # run
+    Window().run()  # run program
